6cb46132925f1256dbf712269ddb1f16fa5256a2
Thomas Sinclair <thomas@icdesign.com.au>
2021-03-21 16:37:51 +1100

    Documenting #to_line_items + santising some input such as extra spaces

--- csv_to_line_items.rb
+++ csv_to_line_items.rb
@@ -6,12 +6,19 @@ module CsvToLineItems
 
   def self.to_line_items(csv_path)
     CSV.read(csv_path, headers: :first_row).map do |row|
-      row = row.to_hash.transform_keys(&:downcase).transform_keys(&:to_sym)
+      # Downcasing all keys to give them a better chance of
+      #   passing through our allow list.
+      row = row.to_hash.transform_keys(&:downcase).transform_keys(&:strip).transform_keys(&:to_sym)
 
+      # Only allowing defined keys
       row.reject! { |k, _| !ALLOWED_KEYS.include?(k) }
+
+      # Ensuring numbers are numbers, and strings don't contain extra spacing
       row.each do |key, value|
         if FLOAT_KEYS.include?(key)
           row[key] = value.to_f
+        else
+          row[key] = value.strip
         end
       end
     end
