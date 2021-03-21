204516ad4fb31e5c23dfa2035ee6ff2be1de2f22
Thomas Sinclair <thomas@icdesign.com.au>
2021-03-21 13:36:51 +1100

    Initial setup with RSpec, .ruby-version and a readme

---
+++ readme.md
@@ -0,0 +1,6 @@
+# ReadyTech code test
+
+## Setup
+
+- Assumes Ruby `2.7.2` (see `.ruby-version`) file.
+- `bundle install` and `rspec .` to see the app.
---
+++ spec/order_spec.rb
@@ -0,0 +1,11 @@
+require './order'
+
+RSpec.describe Order do
+  describe 'can initialise' do
+    context 'can create a new instance of Order' do
+      subject { Order.new.class }
+
+      it { is_expected.to eq(Order) }
+    end
+  end
+end
