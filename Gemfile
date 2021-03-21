7040a6ee25857ec8323694a309861a32170dfc5b
Thomas Sinclair <thomas@icdesign.com.au>
2021-03-21 13:45:49 +1100

    Rubocop + switched out ruby versions for one compatible with rubocop

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
-source 'https://rubygems.org'
+# frozen_string_literal: true
 
-gem 'rspec'
+source "https://rubygems.org"
+
+gem "rspec"
+
+gem "rubocop", "< 0.68"
+gem "rubocop-github"
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,10 @@
-require './order'
+# frozen_string_literal: true
+
+require "./order"
 
 RSpec.describe Order do
-  describe 'can initialise' do
-    context 'can create a new instance of Order' do
+  describe "can initialise" do
+    context "can create a new instance of Order" do
       subject { Order.new.class }
 
       it { is_expected.to eq(Order) }
